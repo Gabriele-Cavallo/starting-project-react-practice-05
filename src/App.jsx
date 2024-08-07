230c87d3f2423d759e5a52d264e18bed4b4bf53a
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-07 14:26:49 +0200

    feat: update components useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import { useState } from 'react';
 import ProjectsSidebar from './components/ProjectsSidebar';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
-import { useState } from 'react';
 function App() {
-  const [isVisible, setIsVisible] = useState(false);
-  function handleClick(){
-    setIsVisible((prevState) => !prevState);
+  const [projectState, setprojectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+  function handleCreateProject(){
+    setprojectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
   };
+  let content;
+  if(projectState.selectedProjectId === null){
+    content = <NewProject />;
+  }else if (projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onCreateProject={handleCreateProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onClick={handleClick} />
-      {isVisible ? <NewProject /> : <NoProjectSelected onClick={handleClick} />}
+      <ProjectsSidebar onCreateProject={handleCreateProject} />
+      {content}
     </main>
   );
 }
