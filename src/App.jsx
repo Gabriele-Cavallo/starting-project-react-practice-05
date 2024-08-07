2cd10d3f49a7e1a001642881cd5312cf62b9f090
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-07 14:06:36 +0200

    feat: add NoProjectSelected component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import ProjectsSidebar from './components/ProjectsSidebar';
 import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected.jsx';
+import { useState } from 'react';
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
   function handleClick(){
-    alert('ciao da App');
+    setIsVisible((prevState) => !prevState);
   };
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar onClick={handleClick} />
-      <NewProject />
+      {isVisible ? <NewProject /> : <NoProjectSelected onClick={handleClick} />}
     </main>
   );
 }
